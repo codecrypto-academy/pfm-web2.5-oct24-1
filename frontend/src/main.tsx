f6bf62c78e8ff14a43bb52e585c4345d24e98d29
Alfredo Delgado L <aadelop@gmail.com>
2024-11-04 13:14:54 +0100

    Added Header and Footer components to the principal tsx file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {Header} from './components/Header'
+import { Footer } from './components/Footer';
 import './index.css'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>Clean react application template</h1>
+    <Header></Header>
+    <p> Text to body</p>
+    <Footer></Footer>
   </StrictMode>,
 )
