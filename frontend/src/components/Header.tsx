36a6a1b405b10b7c5f9ac421ac2220a2a9c16b19
Alfredo Delgado L <aadelop@gmail.com>
2024-11-04 11:34:02 +0100

    Added initial Header component

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Header: React.FC = () => (
+    <header className="d-flex justify-content-between align-items-center bg-dark text-white p-3">
+        <div className="ms-3 fs-4 fw-bold">LOGO</div>
+        <h1 className="m-0 text-center">Ethereum Private Network Creation</h1>
+    <div className="me-3"></div> {/* Espacio vacío para centrar el título */}
+  </header>
+  );
+  
