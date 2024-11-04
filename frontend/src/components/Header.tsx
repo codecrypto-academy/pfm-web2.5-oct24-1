62818f49950f1eb49c215d6b3161c985c5ac1c73
Alfredo Delgado L <aadelop@gmail.com>
2024-11-04 13:14:06 +0100

    Added logo image

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 export const Header: React.FC = () => (
     <header className="d-flex justify-content-between align-items-center bg-dark text-white p-3">
-        <div className="ms-3 fs-4 fw-bold">LOGO</div>
+        <div className="ms-3 d-flex align-items-center">
+            <img src='./src/assets/blockchain-logo.png' alt="Logo" style={{ width: '50px', height: '50px', marginRight: '10px' }} />
+        </div>
         <h1 className="m-0 text-center">Ethereum Private Network Creation</h1>
-    <div className="me-3"></div> {/* Espacio vacío para centrar el título */}
+    <div className="me-3"></div> 
   </header>
   );
   
