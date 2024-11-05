ffeb1756450be2849363091057471c945e5cb011
Orlando Hernandez Celli <ohercelli@gmail.com>
2024-11-05 18:13:08 +0100

    T007 - Network details  (#3)
    * add: add initial project structure with types
    
    * add: add validations for the types created
    
    * feat: add featire networkDetails for the route.ts endpoint that allows obtaining the details of a specific network, such as nodes, IP addresses, roles, among others.
    
    * update lock files
    
    ---------
    
    Co-authored-by: Orloh <ohercelli@protonmail.com>

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,17 +1,19 @@
 import express, { Request, Response, Router } from 'express'
+import { networkDetails } from './functions/networkFunctions'
 
 
 const router: Router = express.Router()
 
 // Network endpoints
-router.get('/networks', (req:Request, res:Response) => { listNetworks(req,res) })
-router.post('/network', (req:Request, res:Response) => { createNetwork(req,res) })
-router.post('/network/:id/start', (req:Request, res:Response) => { startNetwork(req,res) })
-router.post('/network/:id/stop', (req:Request, res:Response) => { stopNetwork(req,res) })
-router.delete('/network/:id', (req:Request, res:Response) => { deleteNetwork(req,res) })
+// TODO router.get('/networks', (req:Request, res:Response) => { listNetworks(req,res) })
+router.get('/network/:id', (req:Request, res:Response) => { networkDetails(req,res) })
+//TODO router.post('/network', (req:Request, res:Response) => { createNetwork(req,res) })
+//TODO router.delete('/network/:id', (req:Request, res:Response) => { deleteNetwork(req,res) })
+//TODO router.post('/network/:id/start', (req:Request, res:Response) => { startNetwork(req,res) })
+//TODO router.post('/network/:id/stop', (req:Request, res:Response) => { stopNetwork(req,res) })
 
-//Node endpoints
-router.post('node/:id', (req:Request, res:Response) => { addNode(req,res) })
-router.delete('node/:id', (req:Request, res:Response) => { deleteNode(req,res) })
+// //Node endpoints
+//TODO router.post('node/:networkid', (req:Request, res:Response) => { addNode(req,res) })
+//TODO router.delete('node/:id', (req:Request, res:Response) => { deleteNode(req,res) })
 
 export default router
