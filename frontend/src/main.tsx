e6531d930201156cf2e47a2653ad44e62af4a213
Alfredo Delgado L <aadelop@gmail.com>
2024-11-05 17:23:24 +0100

    Included NetworkList component in the main app script

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes  } from 'react-router-dom'
 import {App} from './components/App'
+import { NetworkList } from './components/NetworkList'
 import './index.css'
 
 
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')!).render(
       <BrowserRouter>
       <Routes>
         <Route path="/" element= {<App></App>}>
+            <Route path="/networklist" element={<NetworkList></NetworkList>}></Route>
         </Route>
       </Routes>
       </BrowserRouter>
