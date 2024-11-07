4c48e70f9a610ad021e4cf7c83ef77017c87e3fa
Alfredo Delgado L <aadelop@gmail.com>
2024-11-07 13:45:07 +0100

    Added Sidebar menu cmponent

---
+++ frontend/src/components/Sidebar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const Sidebar: React.FC = () =>{
+    return(
+        <aside className="sidebar text-white">
+            <h5 className='text-center'>Menu</h5>
+            <nav>
+                <ul>
+                    <li><Link to="/">New network</Link></li>
+                    <li><Link to="/networklist">Network list</Link></li>
+                </ul>
+            </nav>
+        </aside>
+    )
+}
