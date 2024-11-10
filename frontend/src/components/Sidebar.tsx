adba58861d5516dcc3d6595625482ccca7d15b5a
Alfredo Delgado L <aadelop@gmail.com>
2024-11-10 10:06:29 +0100

    Added conditional dynamic menu specific when enter in a network details

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useParams } from 'react-router-dom';
+
 
 export const Sidebar: React.FC = () =>{
+    const location = useLocation();
+    const { id  } = useParams<{ id: string }>();
     return(
         <aside className="sidebar text-white">
             <h5 className='text-center'>Menu</h5>
@@ -10,6 +13,16 @@ export const Sidebar: React.FC = () =>{
                     <li><Link to="/networklist">Network list</Link></li>
                     <li><Link to="/">New network</Link></li>
                 </ul>
+
+                {location.pathname.includes('/networkdetails') && (
+                    <ul>
+                        <li>
+                            <Link to={`/networkdetails/${id}`}>Network Details: {id}</Link>
+                        </li>
+                        <li>Operations</li>
+                    </ul>
+
+                )}
             </nav>
         </aside>
     )
