1685f9e56e9a4933214e95f2b372cd817a0036dd
Alfredo Delgado L <aadelop@gmail.com>
2024-11-10 08:44:30 +0100

    Added verficator of the networks object before showing the list

--- frontend/src/components/NetworkList.tsx
+++ frontend/src/components/NetworkList.tsx
@@ -10,35 +10,42 @@ export const NetworkList: React.FC = () => {
         });
       });
     }, []);
+    
 
+    if (!networks || networks.length === 0) {
+        return <div>
+                    <h3>Networks List</h3>
+                    <p>No hay redes disponibles.</p>
+            </div>
+      }
 
     return <div>
-        <h3>Networks List</h3>
-        <table className="table">
-        <thead>
-            <tr>
-            <th scope="col">Chain ID</th>
-            <th scope="col">Name</th>
-            <th scope="col">Status</th>
-            <th scope="col">Start</th>
-            <th scope="col">Stop</th>
-            <th scope="col">Restart</th>
-            <th scope="col">Operations</th>
-            </tr>
-        </thead>
-        <tbody>
-        {networks.map((network,index) =>(
-                        <tr key={index}>
-                            <td> {network.chainId}</td>
-                            <td><Link to={`/network/${network.id}`}>{network.id}</Link></td>
-                            <td> Up/Down</td>
-                            <td><button>Start</button></td>
-                            <td><button>Stop</button></td>
-                            <td><button>Restart</button></td>
-                            <td><button>Operations</button></td>
-                        </tr>     
-            ))}
-        </tbody>
-        </table>
-    </div>
+    <h3>Networks List</h3>
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">Chain ID</th>
+          <th scope="col">Name</th>
+          <th scope="col">Status</th>
+          <th scope="col">Start</th>
+          <th scope="col">Stop</th>
+          <th scope="col">Restart</th>
+          <th scope="col">Operations</th>
+        </tr>
+      </thead>
+      <tbody>
+        {networks.map((network, index) => (
+          <tr key={index}>
+            <td>{network.chainId}</td>
+            <td><Link to={`/network/${network.id}`}>{network.id}</Link></td>
+            <td>Up/Down</td>
+            <td><button onClick={async () => startNetwork()} >Start</button></td>
+            <td><button>Stop</button></td>
+            <td><button>Restart</button></td>
+            <td><button>Operations</button></td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
 };
