25291c3a61f9bde0905caf48910c950d8c3bfc24
Orloh <ohercelli@protonmail.com>
2024-11-26 23:38:29 +0100

    feat: front end for the faucet

--- frontend/src/components/Faucet.tsx
+++ frontend/src/components/Faucet.tsx
@@ -1,11 +1,25 @@
 import { useEffect, useState } from "react"
+import { ToastContainer, useToast } from "../components/Toast/Toast";
 import { useParams } from 'react-router-dom'
+
+interface Transaction {
+    address: string
+    block: number
+    txHash: string
+    amount: string
+    balance: string
+    date: string
+}
 
 export const Faucet:React.FC = () => {
     const { id } = useParams<{id: string}>()
-    const [state, setState] = useState<{account: string|null}>({account: null})
+    const [state, setState] = useState<{
+        account: string|null, 
+        balance: string | null}>({
+            account: null,
+            balance: null})
     const [loading, setLoading] = useState<boolean>(false)
-    const [tx, setTx] = useState<object | null>()
+    const [tx, setTx] = useState<Transaction | null>()
     
     useEffect(() =>{
         const ethereum = (window as any).ethereum
@@ -13,40 +27,148 @@ export const Faucet:React.FC = () => {
             alert("Install metamask")
             return
         }
-        ethereum.request({
-            method:"eth_accounts"
-        }).then((accounts: string[]) =>{
-            setState({account: accounts[0]})
+
+        const updateBalance = (account: string) => {
+            ethereum
+                .request({
+                    method: "eth_getBalance",
+                    params: [account, "latest"],
+                })
+                .then((balance: string) => {
+                    const balanceInWei = parseInt(balance, 16);
+                    const ethFactor = 10**18
+                    const balanceInEth = balanceInWei / ethFactor;
+                    console.log(balanceInEth)// Convert Wei to Ether
+                    setState((prevState) => ({
+                        ...prevState,
+                        account,
+                        balance: balanceInEth.toString(),
+                    }));
+                })
+                .catch((error: any) => {
+                    console.error("Error fetching balance:", error);
+                });
+        };
+
+        const handleAccountsChanged = (accounts: string[]) => {
+            if (accounts.length === 0) {
+                alert("No account found");
+                setState((prevState) => ({
+                    ...prevState,
+                    account: null,
+                    balance: "0",
+                }));
+            } else {
+                const account = accounts[0];
+                setState((prevState) => ({ ...prevState, account }));
+                updateBalance(account);
+            }
+        };
+
+        ethereum
+        .request({ method: "eth_accounts" })
+        .then((accounts: string[]) => {
+            if (accounts.length > 0) {
+                handleAccountsChanged(accounts);
+            } else {
+                alert("No account found");
+            }
         })
-        ethereum.on("accountsChanged", 
-            (accounts: string[]) =>{
-                setState({account: accounts[0]})
-            })
+        .catch((error: any) => {
+            console.error("Error fetching accounts:", error);
+        });
+
+        ethereum.on("accountsChanged", handleAccountsChanged);
+
+        return () => {
+        // Clean up the event listener
+            ethereum.removeListener("accountsChanged", handleAccountsChanged);
+        };
     }, [setState])
+
+    const apiUrl = import.meta.env.VITE_API_URL
 
     async function handleClick() {
         setLoading(true)
         if (!state.account){
             return 'There is no account'    
         }
-        const response = await fetch(`http://localhost:3000/network/${id}/faucet/${state.account}`)
-        const transaction = await response.json() as object | null
-        setTx(transaction)
-        setLoading(false)
+        try {
+            const response = await fetch(`${apiUrl}/network/${id}/faucet/${state.account}`,{
+                method: "GET"
+            })
+            const transaction = await response.json() as Transaction | null
+            if(!transaction) {
+                return
+            }                
+            await setTx(transaction)
+            setState((prevState) => ({ ...prevState, balance: transaction.balance.toString()})) 
+            setLoading(false)
+        } catch (error) {
+            
+        }
     }
     
     return(
-        <div>
-            <h2>
-                { state.account ? 
-                    <p>Account: {state.account}  </p> :
-                    <p>No account information</p>
-                }
-            </h2>
-            <button onClick={async () => handleClick()} disabled={loading}>
-                {loading ? 'Waiting for tx...':'Ask for funds' }
-            </button>
-            { tx && <p>Response: {JSON.stringify(tx)} </p>}
+        <div className="container mt-4">
+            <h3>Faucet</h3>
+            <section className="card mb-4">
+                <div className="card-body">
+                    <h5 className="card-title">Account Information</h5>
+                    <ul className="list-unstyled mb-0">
+                        <li className="mb-2">
+                        <strong className="me-2">Address:</strong>
+                            { state.account ? state.account : "Wating for address..." }
+                        </li>
+                        <li className="mb-2">
+                        <strong className="me-2">Balance:</strong>
+                            { state.balance ? state.balance : "Wating for balance..." }
+                        </li>
+                    </ul>
+                        {!loading ? (
+                            <button 
+                                onClick={async () => handleClick()} 
+                                disabled={loading}
+                                className="btn btn-outline-success btn-sm d-flex"
+                            >Ask for funds
+                            </button>
+                            ) : (
+                            <button
+                            disabled={loading}
+                            className="btn btn-warning"
+                            >
+                            Waiting for tx...
+                            </button>)}
+                </div>
+            </section>
+            { tx && 
+            <section className="card mb-4">
+                <div className="card-body">
+                    <h5 className="card-title">Transaction Information</h5>
+                    <ul className="list-unstyled mb-0">
+                        <li className="mb-2">
+                        <strong className="me-2">Block Number:</strong>
+                            {JSON.stringify(tx.block).trim().replace(/^["']|["']$/g, '')}
+                        </li>
+                        <li className="mb-2">
+                        <strong className="me-2">Transaction Hash:</strong>
+                            {JSON.stringify(tx.txHash).trim().replace(/^["']|["']$/g, '')}
+                        </li>
+                        <li className="mb-2">
+                        <strong className="me-2">Balance:</strong>
+                            {JSON.stringify(tx.balance).trim().replace(/^["']|["']$/g, '')}
+                        </li>
+                        <li className="mb-2">
+                        <strong className="me-2">Amount sent:</strong>
+                            {JSON.stringify(tx.amount).trim().replace(/^["']|["']$/g, '')}
+                        </li>
+                        <li>
+                        <strong className="me-2">Timestamp:</strong>
+                            {JSON.stringify(tx.date).trim().replace(/^["']|["']$/g, '')}
+                        </li>
+                    </ul>                    
+                </div>
+            </section>}
         </div>
     )
 }
